760df6c2d4f118c9f2aed71e32a11af4a91a3fd0
sabina-shakiyeva <121431790+sabina-shakiyeva@users.noreply.github.com>
2024-07-09 15:20:42 +0400

    Add files via upload

---
+++ Debtor/Debtor/Debtor.cs
@@ -0,0 +1,25 @@
+﻿namespace Debtors;
+
+public class Debtor
+{
+    public Debtor(string fullName, DateTime birthDay, string phone, string email, string address, int debt)
+    {
+        FullName = fullName;
+        BirthDay = birthDay;
+        Phone = phone;
+        Email = email;
+        Address = address;
+        Debt = debt;
+    }
+
+    public string FullName { get; set; }
+    public DateTime BirthDay { get; set; }
+    public string Phone { get; set; }
+    public string Email { get; set; }
+    public string Address { get; set; }
+    public int Debt { get; set; }
+    public override string ToString()
+    {
+        return $"{this.FullName}{this.BirthDay.ToShortDateString()}{this.Phone}{this.Email}{this.Address}{this.Debt}";
+    }
+}
